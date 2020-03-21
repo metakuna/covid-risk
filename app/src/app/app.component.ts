7548cda58ca281112926f9c70b357b9d6fb541d6
Will Howard <w.howard256@gmail.com>
2020-03-21 13:38:06 +0000

    got basic form working

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'covid-risk';
+  dataForm;
+
+  hospRisk = 20;
+  deathRisk = 5;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.dataForm = formBuilder.group({
+      age: ''
+    });
+  }
+
+  ngOnInit(){
+    console.log('mark');
+  }
+
+  submit() {
+    console.log('clicked submit');
+    this.hospRisk = this.dataForm.value.age;
+  }
 }
